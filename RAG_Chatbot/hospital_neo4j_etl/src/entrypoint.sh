56724c9b0d348d1faacb007a7eb0b3e71d751c21
Joachim De Baer <jdebaer@gmail.com>
2024-06-03 18:49:50 -0600

    Adding container entry point script.

---
+++ RAG_Chatbot/hospital_neo4j_etl/src/entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# This runs in the Docker container.
+
+# Run any setup steps or pre-processing tasks here
+echo "Running ETL to move hospital data from csvs to Neo4j..."
+
+# Run the ETL script
+python hospital_bulk_csv_write.py
